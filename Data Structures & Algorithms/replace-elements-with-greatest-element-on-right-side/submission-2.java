a3fdd191ecd6a42160ec0ffee0ae217d2248e91a
bunu23 <94806809+bunu23@users.noreply.github.com>
2026-06-05 12:15:40 -0400

    Add: replace-elements-with-greatest-element-on-right-side - submission-2

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-2.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] replaceElements(int[] arr) {
+       //Right-to-Left Traversal (Suffix Maximum)
+       int n = arr.length;
+        int[] ans = new int[n];
+        int rightMax = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] = rightMax;
+            rightMax = Math.max(rightMax, arr[i]);
+        }
+        return ans;
+    }
+}
